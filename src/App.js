10d620861f3cd28495641b416af40ad13b39b125
David Do <davidqdo@csu.fullerton.edu>
2022-09-06 17:58:35 -0700

    Rain Done Snow Almost

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import {RainDrops, RainDropsBG, SnowDrops} from "./Weather.js";
 /* Weather API */
 const api = {
   key: "2a371191d68e6c42d89404f7437faa0a",
@@ -73,29 +73,20 @@ function App() {
                 (801 <= weather.weather[0].id && weather.weather[0].id <= 804) ? 'cloud-sym' : /* Cloudy Weather */
                 'sun-sym') : 'sun-sym'} /* Default Sunny Weather */> 
               </div>
-              {200 <= weather.weather[0].id && weather.weather[0].id <= 531 &&
-                <div className="rain-drop">
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                  <div className="rain-item"></div>
-                </div>
+              {200 <= weather.weather[0].id && weather.weather[0].id <= 531 && /* Rainy Weather Symbol */
+                <div>{RainDrops()} {RainDropsBG()}</div>
+              }
+              {600 <= weather.weather[0].id && weather.weather[0].id <= 622 && /* Snowy Weather */
+              <div>{SnowDrops()}</div>
+              }
+              {701 <= weather.weather[0].id && weather.weather[0].id <= 781 && /* Hazardous Weather */
+              <div></div>
+              }
+              {weather.weather[0].id === 800 &&                                 /* Sunny / Clear Weather */
+              <div></div>
+              }
+              {801 <= weather.weather[0].id && weather.weather[0].id <= 804 && /* Cloudy Weather */
+              <div></div>
               }
             </div>
         </div>
---
+++ src/Weather.js
@@ -0,0 +1,140 @@
+export function RainDrops() {
+    return(
+        <div className="rain-drop">
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+            <div className="rain-item"></div>
+        </div>
+    );
+}
+
+export function RainDropsBG() {
+    return (
+        <div className="rain-dropbg">
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+            <div className="rain-itembg"></div>
+    </div>
+    );
+}
+
+export function SnowDrops() {
+    return (
+        <div className="snow-drop">
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+            <div className="snow-item"></div>
+    </div>
+    );
+}
